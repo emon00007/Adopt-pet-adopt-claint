e5bba2c80bf628115991308c903553822891ff47
emon00007 <skemon152@gmail.com>
2024-06-09 14:26:50 +0600

    home page Allcatagory completed

--- src/PageComponent/HomePetCategorys/All_Category.jsx
+++ src/PageComponent/HomePetCategorys/All_Category.jsx
@@ -1,12 +1,45 @@
+import { Link } from "react-router-dom";
 import useCategory from "../../Hooks/useCategory";
-
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+   
+} from "@material-tailwind/react";
 const All_Category = () => {
-    const [category]=useCategory()
+    const [categorys]=useCategory()
     
     return (
-        <div>
-            category{category.length}
+        <div className="p-4">
+        <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-10  ">
+            {categorys.map(category => (
+                <Card key={category._id} className="mt-6 ">
+                    <CardHeader color="blue-gray" className="relative h-56">
+                        <img
+                            src={category.petImage}  
+                            alt="pet"
+                            className="w-full h-full object-cover"
+                        />
+                    </CardHeader>
+                    <CardBody>
+                        <Typography variant="h5" color="blue-gray" className="mb-2">
+                            {category.petName}
+                        </Typography>
+                        <Typography>
+                            {category.shortDescription}
+                        </Typography>
+                    </CardBody>
+                    <CardFooter className="pt-0">
+
+                        <Link to={`/pet/${category._id}`}><Button >View Details</Button></Link>
+                    </CardFooter>
+                </Card>
+            ))}
         </div>
+    </div>
     );
 };
 
--- src/PageComponent/HomePetCategorys/Catagory_dog.jsx
+++ src/PageComponent/HomePetCategorys/Catagory_dog.jsx
@@ -1,13 +1,46 @@
+import { Link } from "react-router-dom";
 import useCategory from "../../Hooks/useCategory";
-
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+   
+} from "@material-tailwind/react";
 const Catagory_dog = () => {
     const [category]=useCategory()
         const dogs = category.filter(item=>item.category ==='dog')
     return (
         
-        <div>
-            <h1> dog{dogs.length}</h1>
+        <div className="p-4">
+        <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-10  ">
+            {dogs.map(dog => (
+                <Card key={dog._id} className="mt-6 ">
+                    <CardHeader color="blue-gray" className="relative h-56">
+                        <img
+                            src={dog.petImage}  
+                            alt="pet"
+                            className="w-full h-full object-cover"
+                        />
+                    </CardHeader>
+                    <CardBody>
+                        <Typography variant="h5" color="blue-gray" className="mb-2">
+                            {dog.petName}
+                        </Typography>
+                        <Typography>
+                            {dog.shortDescription}
+                        </Typography>
+                    </CardBody>
+                    <CardFooter className="pt-0">
+
+                        <Link to={`/pet/${dog._id}`}><Button >View Details</Button></Link>
+                    </CardFooter>
+                </Card>
+            ))}
         </div>
+    </div>
     );
 };
 
