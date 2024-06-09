306cca9967b67a1eef6a4ab68c89485d72379259
emon00007 <skemon152@gmail.com>
2024-06-09 14:12:25 +0600

    home page rabbit catagory completed

--- src/PageComponent/HomePetCategorys/Rabbit_category.jsx
+++ src/PageComponent/HomePetCategorys/Rabbit_category.jsx
@@ -1,12 +1,45 @@
+import { Link } from "react-router-dom";
 import useCategory from "../../Hooks/useCategory";
-
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+   
+} from "@material-tailwind/react";
 const Rabbit_category = () => {
     const [category]=useCategory()
-    const rabbit = category.filter(item=>item.category ==='rabbit')
+    const rabbits = category.filter(item=>item.category ==='rabbit')
     return (
-        <div>
-           rabbit {rabbit.length}
+        <div className="p-4">
+        <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-10  ">
+            {rabbits.map(rabbit => (
+                <Card key={rabbit._id} className="mt-6 ">
+                    <CardHeader color="blue-gray" className="relative h-56">
+                        <img
+                            src={rabbit.petImage}  // Assuming the pet data has an imageUrl field
+                            alt="pet"
+                            className="w-full h-full object-cover"
+                        />
+                    </CardHeader>
+                    <CardBody>
+                        <Typography variant="h5" color="blue-gray" className="mb-2">
+                            {rabbit.petName}
+                        </Typography>
+                        <Typography>
+                            {rabbit.shortDescription}
+                        </Typography>
+                    </CardBody>
+                    <CardFooter className="pt-0">
+
+                        <Link to={`/pet/${rabbit._id}`}><Button >View Details</Button></Link>
+                    </CardFooter>
+                </Card>
+            ))}
         </div>
+    </div>
     );
 };
 
