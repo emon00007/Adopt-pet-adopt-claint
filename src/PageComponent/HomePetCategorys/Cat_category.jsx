1304d0e2104e6c38084ec27c95b1a244fc92c9c2
emon00007 <skemon152@gmail.com>
2024-06-09 13:20:01 +0600

    home page cat catagory completed

--- src/PageComponent/HomePetCategorys/Cat_category.jsx
+++ src/PageComponent/HomePetCategorys/Cat_category.jsx
@@ -1,11 +1,44 @@
+import { Link } from "react-router-dom";
 import useCategory from "../../Hooks/useCategory";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
 
+} from "@material-tailwind/react";
 const Cat_category = () => {
-    const [category]=useCategory()
-    const cat = category.filter(item=>item.category ==='cat')
+    const [category] = useCategory()
+    const cats = category.filter(item => item.category === 'cat')
     return (
-        <div>
-            cat{cat.length}
+        <div className="p-4">
+            <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-10  ">
+                {cats.map(cat => (
+                    <Card key={cat._id} className="mt-6 ">
+                        <CardHeader color="blue-gray" className="relative h-56">
+                            <img
+                                src={cat.petImage}  // Assuming the pet data has an imageUrl field
+                                alt="pet"
+                                className="w-full h-full object-cover"
+                            />
+                        </CardHeader>
+                        <CardBody>
+                            <Typography variant="h5" color="blue-gray" className="mb-2">
+                                {cat.petName}
+                            </Typography>
+                            <Typography>
+                                {cat.shortDescription}
+                            </Typography>
+                        </CardBody>
+                        <CardFooter className="pt-0">
+
+                            <Link to={`/pet/${cat._id}`}><Button >View Details</Button></Link>
+                        </CardFooter>
+                    </Card>
+                ))}
+            </div>
         </div>
     );
 };
